168eb2c61ca0e3046c0c691461a4bd172a30b0c2
Anton Boyko <boyko.anton.mp@gmail.com>
2017-05-02 16:42:19 +0300

    Logic
    nothing important

--- chat/Form1.Designer.cs
+++ chat/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -36,7 +37,6 @@
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -53,6 +53,15 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Login";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "     Input your nick:";
             // 
             // button2
             // 
@@ -72,6 +81,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
@@ -94,7 +104,7 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Enabled = false;
+            this.richTextBox1.ReadOnly = true;
             this.richTextBox1.Location = new System.Drawing.Point(6, 19);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(548, 205);
@@ -120,15 +130,6 @@
             this.textBox2.Size = new System.Drawing.Size(457, 43);
             this.textBox2.TabIndex = 0;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 22);
-            this.label1.Name = "nickLabel";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "     Input your nick:";
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -136,7 +137,7 @@
             this.ClientSize = new System.Drawing.Size(584, 361);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.Name = "ChatForm";
+            this.Name = "Form1";
             this.Text = "Chat based on UDP by A.B.";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
--- chat/Form1.cs
+++ chat/Form1.cs
@@ -28,7 +28,36 @@ namespace chat
         {
             InitializeComponent();
 
-            groupIP = IPAddress.Parse(HOST);
+            groupIP = IPAddress.Parse(HOST); //convetr ip from string to type "IPAddress"
+        }
+
+        private void button1_Click(object sender, EventArgs e) //click on login button
+        {
+            nickname = textBox1.Text; //get nickname from textbox
+            textBox1.ReadOnly = true; //ban editing textbox with username
+            
+            try
+            {
+                //client = new UdpClient(LOCALPORT); //bind client on LOCALPORT
+                //
+                //client.JoinMulticastGroup(groupIP, TTL); //conect to group distribution
+                //
+                //Task receiveTask = new Task(receiveMsg); //creating async operation for receiving messages
+                //receiveTask.Start(); // start receiveTask asynk operation
+                //
+                //string message = nickname + " now online"; //creating a string about new member in chat
+                //byte[] data = Encoding.Unicode.GetBytes(message); //creating buffer data and converting message into that buffer
+                //client.Send(data, data.Length, HOST, REMOTEPORT); //sending our message in buffer of bytes to REMOTEPORT
+
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                textBox2.Enabled = true;
+            }   
+            catch
+            {   
+
+            }
         }
     }
 }
